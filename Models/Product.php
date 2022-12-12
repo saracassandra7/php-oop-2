c3f3f27068088aaa37aa607ca038fc753a5e1263
saracassandra7 <saracassandra7@gmail.com>
2022-12-12 15:53:30 +0100

    [add] basic product class

---
+++ Models/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+class Product{
+  public $category;
+  public $name;
+  public $image;
+  public $brand;
+  public $price;
+  public $is_available = true;
+
+  public function __construct($_category, $_name, $_image, $_brand, $_price)
+  {
+    $this->category = $_category;
+    $this->name = $_name;
+    $this->image = $_image;
+    $this->brand = $_brand;
+    $this->price = $_price;
+  }
+}
+
+?>
